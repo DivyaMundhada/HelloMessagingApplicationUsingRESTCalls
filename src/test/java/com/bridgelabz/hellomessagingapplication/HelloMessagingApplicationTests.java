6efe4bd9c6ce1aa70955d39db8632ac44c75a739
DivyaMundhada <divyamundada9@gmail.com>
2021-08-12 20:45:50 +0530

    [Divya] Created Hello Messaging Application Using REST API Calls.

---
+++ src/test/java/com/bridgelabz/hellomessagingapplication/HelloMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.hellomessagingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMessagingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
