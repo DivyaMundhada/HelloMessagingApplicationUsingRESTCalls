f957918f54a815ec8d52f7f35f15ed9f59e251b5
DivyaMundhada <divyamundada9@gmail.com>
2021-08-12 21:32:15 +0530

    [Divya] Added: GET Request Method and passed name as Path Variable

--- src/main/java/com/bridgelabz/hellomessagingapplication/controller/HelloMessageController.java
+++ src/main/java/com/bridgelabz/hellomessagingapplication/controller/HelloMessageController.java
@@ -1,3 +1,4 @@
+
 package com.bridgelabz.hellomessagingapplication.controller;
 
 import org.springframework.web.bind.annotation.*;
@@ -5,13 +6,22 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/welcome")
 public class HelloMessageController {
+
+    //UC-1 GET Request Method to display a message
     @GetMapping("/get")
     public String hello() {
         return "Hello From Bridgelabz.";
     }
 
+    //UC-2 GET Request Method to pass name as query parameters
     @GetMapping("/queryparam")
     public String hello(@RequestParam(value = "name" ) String name ) {
+        return "Hello " + name + " from Bridgelabz.";
+    }
+
+    //UC-3 GET Request Method to pass name as Path Variable
+    @GetMapping("/hello/{name}")
+    public String helloMessage(@PathVariable("name") String name){
         return "Hello " + name + " from Bridgelabz.";
     }
 }
